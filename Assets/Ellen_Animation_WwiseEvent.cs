aaecb87e2b0af299fad6a47d6a70b4e0ce1a2011
exectra <157939496+exectra@users.noreply.github.com>
2026-03-01 15:09:14 +0800

    Fixed Footsteps in WWise and tried implementing Raycast for ground detection

--- Assets/Ellen_Animation_WwiseEvent.cs
+++ Assets/Ellen_Animation_WwiseEvent.cs
@@ -4,14 +4,31 @@ using UnityEngine;
 
 public class Ellen_Animation_WwiseEvent : MonoBehaviour
 {
-
     public AK.Wwise.Event Play_Footsteps;
 
-public void PlayRunFootsteps()
-{
-    Debug.Log ("Footsteps Played");
-    Play_Footsteps.Post(gameObject);
-}
+    public void PlayRunFootsteps()
+    {
+        string surface = DetectSurface();
+        AkUnitySoundEngine.SetSwitch("FoostepsState", surface, gameObject);
 
- 
+        Debug.Log ("Footsteps Played");
+        Play_Footsteps.Post(gameObject);
+    }
+    string DetectSurface()
+    {
+        RaycastHit hit;
+
+        Vector3 origin = transform.position + Vector3.up * 0.2f;
+
+        Debug.DrawRay(origin, Vector3.down * 3f, Color.red);
+
+        if (Physics.Raycast(origin, Vector3.down, out hit, 3f))
+        {
+            Debug.Log("Hit: " + hit.collider.name + " | Tag: " + hit.collider.tag);
+            return hit.collider.tag;
+        }
+
+        return "Grass";
+    }
+
 }
